0ccb18b0fa60ea227b01ff0edcfd677a010a273b
binllionaire <bug20474@gmail.com>
2022-11-21 14:04:46 +0900

    다리 길이 입력받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const {INPUT_MESSAGE} = require("./util/constants");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    MissionUtils.Console.readLine(INPUT_MESSAGE.BRIDGE_SIZE, (bridgeSize) => {
+      
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
---
+++ src/util/constants.js
@@ -0,0 +1,7 @@
+const INPUT_MESSAGE = {
+    BRIDGE_SIZE : '다리의 길이를 입력해주세요.\n' 
+  }
+
+  module.exports = {
+    INPUT_MESSAGE
+  }
